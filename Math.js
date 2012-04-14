f3e0ee3f2bffee3c674ddff3e1d363c03b7574ec
D. Rimron <darran@xalior.com>
2012-04-14 23:08:04 +0100

    Storing state between refactors

---
+++ Math.js
@@ -0,0 +1,14 @@
+/*
+ * This is not a Math Library,
+ *  (this is a Math Library)
+ *  (this is just a tribute)
+ * This is just a set of helpers, to make code easier to maintain.
+ *
+ * WTFPL
+ *
+ * D. Rimron <darran@xalior.com>
+ */
+
+function randomint(floor, ceil) {
+    return Math.floor((Math.random()*ceil)+floor);
+}
