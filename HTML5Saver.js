fa227f81cff9d7996d6650466e14354dee8f707e
D. Rimron <darran@xalior.com>
2012-04-15 01:42:02 +0100

    BuBye djsex faker
    -Dx

--- HTML5Saver.js
+++ HTML5Saver.js
@@ -1,107 +1,18 @@
-/* Simple polyfil to requestAnimationFrame
- * By Paul Irish http://paulirish.com/2011/requestanimationframe-for-smart-animating/
- */
-(function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = 
-          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
- 
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
- 
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-}());
-/* Simple JavaScript Inheritance
- * By John Resig http://ejohn.org/
- * MIT Licensed.
- */
-// Inspired by base2 and Prototype
-(function(){
-	var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
-	// The base Class implementation (does nothing)
-	this.Class = function(){};
-
-	// Create a new Class that inherits from this class
-	Class.extend = function(prop) {
-		var _super = this.prototype;
-
-		// Instantiate a base class (but only create the instance,
-		// don't run the init constructor)
-		initializing = true;
-		var prototype = new this();
-		initializing = false;
-
-		// Copy the properties over onto the new prototype
-		for (var name in prop) {
-			// Check if we're overwriting an existing function
-			prototype[name] = typeof prop[name] == "function" &&
-				typeof _super[name] == "function" && fnTest.test(prop[name]) ?
-				(function(name, fn){
-					return function() {
-						var tmp = this._super;
-
-						// Add a new ._super() method that is the same method
-						// but on the super-class
-						this._super = _super[name];
-
-						// The method only need to be bound temporarily, so we
-						// remove it when we're done executing
-						var ret = fn.apply(this, arguments);
-						this._super = tmp;
-
-						return ret;
-					};
-				})(name, prop[name]) :
-				prop[name];
-		}
-
-		// The dummy class constructor
-		function Class() {
-			// All construction is actually done in the init method
-			if ( !initializing && this.init )
-				this.init.apply(this, arguments);
-		}
-
-		// Populate our constructed prototype object
-		Class.prototype = prototype;
-
-		// Enforce the constructor to be what we expect
-		Class.prototype.constructor = Class;
-
-		// And make this class extendable
-		Class.extend = arguments.callee;
-
-		return Class;
-	};
-})();
-function randomint(floor, ceil) {
-    return Math.floor((Math.random()*ceil)+floor);
-}
-	
-
-var HTML5Saver = Class.extend({
+var HTML5Saver = djsex.Class.extend({
 	init: function(container,options){
         this.container = container ? container : document.getElementsByTagName('body')[0];
+
+        this.red = 255;
+        this.green = 255;
+        this.blue = 255;
 	},
 
     render: function(now){
-
-        this.container.innerHTML='<img src="img/'+images[randomint(0,images.length)].name +'">';
+        this.draw();
 	},
+
+    draw: function() {
+    },
   
 });
 
